d7335c744143aecbdb6b2cad5ade7f5a8ed8dba0
kbshin <amazing1997@naver.com>
2024-12-11 17:08:30 +0900

    Initial Commit

---
+++ plipli/plipliApp.swift
@@ -0,0 +1,17 @@
+//
+//  plipliApp.swift
+//  plipli
+//
+//  Created by KIBEOM SHIN on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct plipliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
